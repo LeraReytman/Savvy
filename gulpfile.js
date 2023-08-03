793699d888858a893361faba95d94da66361e726
Lera Reytman <lerareytman@gmail.com>
2023-08-03 14:01:20 +0300

    rem

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
-    'node_modules/swiper/swiper-bundle.min.js',
+    // 'node_modules/swiper/swiper-bundle.min.js',
     // 'node_modules/mixitup/dist/mixitup.js',
     // 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     // 'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
