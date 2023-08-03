2a26bfb986fdd90c4223b625226ea4c1268cb675
Lera Reytman <lerareytman@gmail.com>
2023-08-03 13:22:11 +0300

    decision page

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,4 @@
 $(function(){
 
-  let swiper = new Swiper(".swiper-one", {
-
-    slidesPerView: 2,
-
-    spaceBetween: 114,
-
-    speed: 1000,
-
-    centeredSlides: true,
-
-    autoplay: {
-
-      delay: 2000,
-
-    }, 
-
-  });
   
 });
