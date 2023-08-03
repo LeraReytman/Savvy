dc04a3a56c6e0f757f0360b1a37dacad798bd6e7
lolzhest <sugans204244@gmail.com>
2023-08-04 01:24:35 +0300

    anim scroll

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,53 @@ $(function(){
     });
   
     scroll.on('scroll', (args) => {
-  
+
+      let servicesOne = $('.services__bg-one--one').offset().top + 200;
+      let servicesTwo = $('.services__bg-one--two').offset().top + 200;
+      let servicesThree = $('.services__bg-one--three').offset().top + 200;
+
+      let wScroll = $('.decision-wrapper').offset().top;
+      wScroll = Math.abs(wScroll);
+      const height = $(window).height();
+
+      console.log(wScroll)
+
+      console.log(servicesTwo + height)
+
+      const servicesOnePos = (wScroll + height - servicesOne);
+      const servicesTwoPos = (wScroll + height - servicesTwo);
+      const servicesThreePos = (wScroll + height - servicesThree);
+
+      if (wScroll >= servicesOne) {
+
+        $('.services__bg-one--one').css({
+
+          'transform' : 'rotate(' + (servicesOnePos) * 0.2 + 'deg)'
+
+        });
+
+      }
+
+      if (wScroll >= servicesTwo) {
+
+        $('.services__bg-one--two').css({
+
+          'transform' : 'rotate(' + (servicesTwoPos) * 0.2 + 'deg)'
+
+        });
+
+      }
+
+      if (wScroll >= servicesThree) {
+
+        $('.services__bg-one--three').css({
+
+          'transform' : 'rotate(' + (servicesThreePos) * 0.2 + 'deg)'
+
+        });
+
+      }
+
       if(typeof args.currentElements['num-one'] === 'object') {
   
         $('.why__numb--one').addClass('why__numb--visible')
